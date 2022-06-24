81664e3d9a9218f2978817bed6385efeb6d000d9
VitaliiPoliatykin <102904854+VitaliiPoliatykin@users.noreply.github.com>
2022-06-25 00:55:20 +0300

    Начало кода

---
+++ js/customer-reviews.js
@@ -0,0 +1,10 @@
+let offset = 0; // смещение от левого края
+const comment = document.querySelector(`.customer-reviews__zone`);
+
+document.querySelector(`btn-next`).addEventListener(`click`, function () {
+        loop: false
+      });
+
+document.querySelector(`btn-previous`).addEventListener(`click`, function () {
+        loop: false
+      });
